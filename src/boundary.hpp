ce4e7d58da725df85f6a4163437bb27c6ac75439
Lorenzo Fonnesu <75188388+Fonzy-01@users.noreply.github.com>
2023-11-25 19:26:56 +0100

    Few adjustements

--- src/boundary.hpp
+++ src/boundary.hpp
@@ -19,11 +19,11 @@ public:
 class BounceBack : Boundary //<q>
 {
 public:
-    /*
+    /**
     * @param f array containing distribution functions of a node
     * @param fEq array containing equilibrium distribution functions of a node
     * @param tConst deltaT/tau
-    * @param tConj: 1-tConst
+    * @param tConj 1-tConst
     * @return the post-bounce distribution functions
     */
     std::vector<double> calcBounce(const std::vector<double> &f, const std::vector<double> &fEq, double tConst, double tConj) const override;
@@ -31,7 +31,15 @@ public:
     /*
     std::array<double, q> calcBounce(const std::array<double, q> &f, const std::array<double, q> &fEq, double tConst, double tConj) const override
     {
-        
+        std::array<double, q> fStar;
+
+        for(size_t i=0; i<q; i++){
+            // The idea is to look, with the help of c values and a getter of single nodes in the lattice (maybe the coord of the current
+            // node too), if the current node neighbours are fluid or not
+            // If they are, we can proceed as BGK, otherwise following the rules of BB method
+            // Another option is to call this method singularly for the nodes that aren't fluid
+            // Finally, please note that there are some cases (for example corners) in which this method doesn't work in the same way
+        }
     }
     */
 
--- src/collisionModel.hpp
+++ src/collisionModel.hpp
@@ -19,11 +19,11 @@ public:
 class BGK : public CollisionModel //<q>
 {
 public:
-    /*
+    /**
     * @param f array containing distribution functions of a node
     * @param fEq array containing equilibrium distribution functions of a node
     * @param tConst deltaT/tau
-    * @param tConj: 1-tConst
+    * @param tConj 1-tConst
     * @return the post-collision distribution functions
     */
     std::vector<double> calcCollision(const std::vector<double> &f, const std::vector<double> &fEq, double tConst, double tConj) const override;
@@ -32,12 +32,14 @@ public:
     std::array<double, q> calcCollision(const std::array<double, q> &f, const std::array<double, q> &fEq, double tConst, double tConj) const override
     {
         std::array<double, q> fStar;
+
         // This loop works for all DxQy, we can improve it deciding to remove D3Q19, because all the others y can be divided by 3
         for(size_t i=0; i<q; i++){
             fStar[i] = f[i] * tConj + fEq[i] * tConst;
         }
 
         return fStar;
+        // To assign correctly all the new distribution functions, we can use c values
     }
     */
 
