351b177ffa26b48319750e10ae9fc51102502f99
Andrea Grassi <andrea10.grassi@mail.polimi.it>
2023-11-29 15:13:40 +0000

    added mtx format

--- src/Python_scripts/translate.py
+++ src/Python_scripts/translate.py
@@ -1,5 +1,5 @@
 #comando per runnare
-#python src/Python_scripts/translate.py immagine_input.jpg nome_output.txt
+#python src/Python_scripts/translate.py immagine_input.jpg nome_output
 
 import sys
 from PIL import Image
@@ -24,27 +24,41 @@ args=sys.argv
 img = Image.open(f"resources/img/{args[1]}")
 
 length, height =img.size
-with open(f"resources/patterns/{args[2]}", "w") as file:
-    for x in range(height):
-        for y in range(length):
-            pixel=img.getpixel((y,x))
-            #print(f"Pixel alla posizione ({x}, {y}): {pixel}")
-            if(is_boundary(pixel)) :
-                file.write("B")
-                #file.write("\033[30ml\033[0m")
-            elif(is_fluid(pixel)):
-                file.write("W")
-                #file.write("\033[37ml\033[0m")
-            elif(is_solid(pixel)):
-                file.write("S")
-                #file.write("\033[34ml\033[0m")
-            else: 
-                file.write("?")
-        file.write("\n")        
-        #file.write("                                                                                                                                                                           ")
-          # Blu
-          # Nero
-        
+nonzeros = 0
+
+with open(f"resources/patterns/{args[2]}.txt", "w") as file:
+    with open(f"resources/patterns/{args[2]}.mtx","w") as mtx:
+        mtx.write("%%MatrixMarket matrix coordinate real general\n")
+        mtx.write("\n")
+        for x in range(height):
+            for y in range(length):
+                pixel=img.getpixel((y,x))
+                #print(f"Pixel alla posizione ({x}, {y}): {pixel}")
+                if(is_boundary(pixel)) :
+                    file.write("B")
+                    nonzeros=nonzeros+1
+                    mtx.write(f"{x} {y} 1\n")
+                    #file.write("\033[30ml\033[0m")
+                elif(is_fluid(pixel)):
+                    file.write("W")
+                    #file.write("\033[37ml\033[0m")
+                elif(is_solid(pixel)):
+                    file.write("S")
+                    mtx.write(f"{x} {y} 3\n")
+                    #file.write("\033[34ml\033[0m")
+                else: 
+                    file.write("?")
+            file.write("\n")        
+            #file.write("                                                                                                                                                                           ")
+            # Blu
+            # Nero
+
+with open(f"resources/patterns/{args[2]}.mtx","r") as mtx:  
+    lines=mtx.readlines()
+lines[1]=f"{length} {height} {nonzeros}\n"
+
+with open(f"resources/patterns/{args[2]}.mtx","w") as mtx:
+    mtx.writelines(lines)
 
 
 img.close
