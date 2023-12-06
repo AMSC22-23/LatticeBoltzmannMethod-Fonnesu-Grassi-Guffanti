cf4f6299f1b91847da3ce45769930a7410463dad
Andrea Grassi <andrea10.grassi@mail.polimi.it>
2023-12-06 09:26:04 +0000

    small boundary recognition fix

--- src/lbm/latticeReader.cpp
+++ src/lbm/latticeReader.cpp
@@ -116,36 +116,40 @@ bool LatticeReader2D::read_lattice_structure(LatticeGrid2D& lattice,
         
 
         //WALLS
-        if(j!=0){
-            if( (lattice[i+1][j].is_generic_boundary() && lattice[i-1][j].is_generic_boundary()) &&
-                (!lattice[i][j-1].is_generic_boundary())){
-                    std::get<2>(boundary) = RIGHT_WALL_2D;
-                    std::cout << i << " " << j << " " << std::get<2>(boundary) << std::endl;
-                    continue;
+        if(i!=0 && i!=(height-1)){
+            if(j!=0 ){
+                if( (lattice[i+1][j].is_generic_boundary() && lattice[i-1][j].is_generic_boundary()) &&
+                    (!lattice[i][j-1].is_generic_boundary())){
+                        std::get<2>(boundary) = RIGHT_WALL_2D;
+                        std::cout << i << " " << j << " " << std::get<2>(boundary) << std::endl;
+                        continue;
+                }
+            }
+            if(j!=(width-1)){ // il meno uno c'è? punto di domanda FIXME:
+                if( (lattice[i+1][j].is_generic_boundary() && lattice[i-1][j].is_generic_boundary()) &&
+                    (!lattice[i][j+1].is_generic_boundary())){
+                        std::get<2>(boundary) = LEFT_WALL_2D;
+                        std::cout << i << " " << j << " " << std::get<2>(boundary) << std::endl;
+                        continue;
+                }
             }
         }
-        if(j!=(width-1)){ // il meno uno c'è? punto di domanda FIXME:
-            if( (lattice[i+1][j].is_generic_boundary() && lattice[i-1][j].is_generic_boundary()) &&
-                (!lattice[i][j+1].is_generic_boundary())){
-                    std::get<2>(boundary) = LEFT_WALL_2D;
-                    std::cout << i << " " << j << " " << std::get<2>(boundary) << std::endl;
-                    continue;
+        if(j!=0 && j!=(width-1)){
+            if(i!=0){
+                if( (lattice[i][j+1].is_generic_boundary() && lattice[i][j-1].is_generic_boundary()) &&
+                    (!lattice[i-1][j].is_generic_boundary())){
+                        std::get<2>(boundary) = BOTTOM_WALL_2D;
+                        std::cout << i << " " << j << " " << std::get<2>(boundary) << std::endl;
+                        continue;
+                }
             }
-        }
-        if(i!=0){
-            if( (lattice[i][j+1].is_generic_boundary() && lattice[i][j-1].is_generic_boundary()) &&
-                (!lattice[i-1][j].is_generic_boundary())){
-                    std::get<2>(boundary) = UPPER_WALL_2D;
-                    std::cout << i << " " << j << " " << std::get<2>(boundary) << std::endl;
-                    continue;
-            }
-        }
-        if(i!=(height-1)){
-            if( (lattice[i][j+1].is_generic_boundary() && lattice[i][j-1].is_generic_boundary()) &&
-                (!lattice[i+1][j].is_generic_boundary())){
-                    std::get<2>(boundary) = BOTTOM_WALL_2D;
-                    std::cout << i << " " << j << " " << std::get<2>(boundary) << std::endl;
-                    continue;
+            if(i!=(height-1)){
+                if( (lattice[i][j+1].is_generic_boundary() && lattice[i][j-1].is_generic_boundary()) &&
+                    (!lattice[i+1][j].is_generic_boundary())){
+                        std::get<2>(boundary) = UPPER_WALL_2D;
+                        std::cout << i << " " << j << " " << std::get<2>(boundary) << std::endl;
+                        continue;
+                }
             }
         }
     }
