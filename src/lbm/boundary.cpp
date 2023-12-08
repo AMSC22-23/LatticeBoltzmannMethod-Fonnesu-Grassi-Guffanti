7dd8f416b2cbcacd2a017747345f2dcc41ce62cb
Lorenzo Fonnesu <75188388+Fonzy-01@users.noreply.github.com>
2023-12-08 19:20:05 +0100

    directions corrected

--- src/lbm/boundary.cpp
+++ src/lbm/boundary.cpp
@@ -65,23 +65,23 @@ void NEBB::calc_right_wall_bounce(LatticeNode<2> &node) const
 
     node.set_population(3) = f[1] - two_thirds*rho*node.get_u()[0];
 
-    node.set_population(6) = f[5] + one_half*(f[2]-f[4]) - 
+    node.set_population(7) = f[5] + one_half*(f[2]-f[4]) - 
                              one_sixth*rho*node.get_u()[0] - 
                              one_half*rho*node.get_u()[1];
 
-    node.set_population(7) = f[8] - one_half*(f[2]-f[4]) - 
+    node.set_population(6) = f[8] - one_half*(f[2]-f[4]) - 
                              one_sixth*rho*node.get_u()[0] + 
                              one_half*rho*node.get_u()[1];
 }
 
 void NEBB::calc_top_left_corner_bounce(LatticeNode<2> &node) const
 {
-    node.set_population(1) = node.get_populations()[2] + two_thirds * node.get_rho() * node.get_u()[0];
-    node.set_population(4) = node.get_populations()[3] - two_thirds * node.get_rho() * node.get_u()[1];
-    node.set_population(8) = node.get_populations()[7] + one_sixth * node.get_rho() * (node.get_u()[0] - node.get_u()[1]);
+    node.set_population(1) = node.get_populations()[3] + two_thirds * node.get_rho() * node.get_u()[0];
+    node.set_population(4) = node.get_populations()[2] - two_thirds * node.get_rho() * node.get_u()[1];
+    node.set_population(8) = node.get_populations()[6] + one_sixth * node.get_rho() * (node.get_u()[0] - node.get_u()[1]);
 
     node.set_population(5) = 0.0;
-    node.set_population(6) = 0.0;
+    node.set_population(7) = 0.0;
 
     node.set_population(0) = node.get_rho() 
                             - node.get_populations()[1] - node.get_populations()[3] - node.get_populations()[2] - node.get_populations()[4]
@@ -90,11 +90,11 @@ void NEBB::calc_top_left_corner_bounce(LatticeNode<2> &node) const
 
 void NEBB::calc_top_right_corner_bounce(LatticeNode<2> &node) const
 {
-    node.set_population(2) = node.get_populations()[1] - two_thirds * node.get_rho() * node.get_u()[0];
+    node.set_population(3) = node.get_populations()[1] - two_thirds * node.get_rho() * node.get_u()[0];
     node.set_population(4) = node.get_populations()[3] - two_thirds * node.get_rho() * node.get_u()[1];
-    node.set_population(6) = node.get_populations()[5] - one_sixth * node.get_rho() * (node.get_u()[0] + node.get_u()[1]);
+    node.set_population(7) = node.get_populations()[5] - one_sixth * node.get_rho() * (node.get_u()[0] + node.get_u()[1]);
 
-    node.set_population(7) = 0.0;
+    node.set_population(6) = 0.0;
     node.set_population(8) = 0.0;
 
     node.set_population(0) = node.get_rho() 
@@ -104,11 +104,11 @@ void NEBB::calc_top_right_corner_bounce(LatticeNode<2> &node) const
 
 void NEBB::calc_bottom_left_corner_bounce(LatticeNode<2> &node) const
 {
-    node.set_population(1) = node.get_populations()[2] - two_thirds * node.get_rho() * node.get_u()[0];
-    node.set_population(3) = node.get_populations()[4] - two_thirds * node.get_rho() * node.get_u()[1];
-    node.set_population(5) = node.get_populations()[6] + one_sixth * node.get_rho() * (node.get_u()[0] + node.get_u()[1]);
+    node.set_population(1) = node.get_populations()[3] - two_thirds * node.get_rho() * node.get_u()[0];
+    node.set_population(2) = node.get_populations()[4] - two_thirds * node.get_rho() * node.get_u()[1];
+    node.set_population(5) = node.get_populations()[7] + one_sixth * node.get_rho() * (node.get_u()[0] + node.get_u()[1]);
 
-    node.set_population(7) = 0.0;
+    node.set_population(6) = 0.0;
     node.set_population(8) = 0.0;
 
     node.set_population(0) = node.get_rho() 
@@ -118,12 +118,12 @@ void NEBB::calc_bottom_left_corner_bounce(LatticeNode<2> &node) const
 
 void NEBB::calc_bottom_right_corner_bounce(LatticeNode<2> &node) const
 {
-    node.set_population(2) = node.get_populations()[1] - two_thirds * node.get_rho() * node.get_u()[0];
-    node.set_population(3) = node.get_populations()[4] + two_thirds * node.get_rho() * node.get_u()[1];
-    node.set_population(6) = node.get_populations()[8] - one_sixth * node.get_rho() * (node.get_u()[0] - node.get_u()[1]);
+    node.set_population(3) = node.get_populations()[1] - two_thirds * node.get_rho() * node.get_u()[0];
+    node.set_population(2) = node.get_populations()[4] + two_thirds * node.get_rho() * node.get_u()[1];
+    node.set_population(6) = node.get_populations()[8] - one_sixth * node.get_rho() * (node.get_u()[0] + node.get_u()[1]);
 
     node.set_population(5) = 0.0;
-    node.set_population(6) = 0.0;
+    node.set_population(7) = 0.0;
 
     node.set_population(0) = node.get_rho() 
                             - node.get_populations()[1] - node.get_populations()[2] - node.get_populations()[3] - node.get_populations()[4]
