e8e4796dfaad4804a2b7adcaa46832c6eb26a454
Luca Guffanti <luca.guffanti.000@gmail.com>
2023-12-18 12:05:17 +0000

    Add check that openmp is enabled

--- src/lbm/lbm.cpp
+++ src/lbm/lbm.cpp
@@ -82,39 +82,43 @@ void lbm::compute(const double time)
 
 void lbm::perform_strong_scaling_test()
 {
-    double dt = re * (0.2 * 100 /re)/(100.0 * 100.0);
-    const std::size_t n_iter = floor(5.0/dt);
-    std::vector<std::tuple<std::size_t, double>> results_table;
+    #ifdef _OPENMP
+        double dt = re * (0.2 * 100 /re)/(100.0 * 100.0);
+        const std::size_t n_iter = floor(5.0/dt);
+        std::vector<std::tuple<std::size_t, double>> results_table;
 
-    std::size_t max_num_threads = omp_get_max_threads();
-    std::cout << "=====STRONG SCALING TEST=====" << std::endl;
-    std::cout << "Available OMP threads: " << max_num_threads << std::endl;
+        std::size_t max_num_threads = omp_get_max_threads();
+        std::cout << "=====STRONG SCALING TEST=====" << std::endl;
+        std::cout << "Available OMP threads: " << max_num_threads << std::endl;
 
-    for (std::size_t num_threads = 1; num_threads <= max_num_threads; num_threads++)
-    {
-        std::cout << "Testing with " << num_threads << " thread(s): ";
-        lattice_ptr->set_omp_num_threads() = num_threads;
-        auto start_time = std::chrono::high_resolution_clock::now();
-        for (std::size_t i = 0; i <= n_iter; i++)
+        for (std::size_t num_threads = 1; num_threads <= max_num_threads; num_threads++)
         {
+            std::cout << "Testing with " << num_threads << " thread(s): ";
+            lattice_ptr->set_omp_num_threads() = num_threads;
+            auto start_time = std::chrono::high_resolution_clock::now();
+            for (std::size_t i = 0; i <= n_iter; i++)
             {
-                // REINITIALIZATION OF INLET FIELDS
-                lattice_ptr->set_inlets(i);
+                {
+                    // REINITIALIZATION OF INLET FIELDS
+                    lattice_ptr->set_inlets(i);
 
-                lattice_ptr->perform_simulation_step();                    
-            }    
+                    lattice_ptr->perform_simulation_step();                    
+                }    
+            }
+            auto end_time = std::chrono::high_resolution_clock::now();
+            auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
+            std::cout << duration.count()/1000.0 << " ms" << std::endl;
+            
+            results_table.push_back({num_threads, duration.count()/1000.0});
         }
-        auto end_time = std::chrono::high_resolution_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
-        std::cout << duration.count()/1000.0 << " ms" << std::endl;
-        
-        results_table.push_back({num_threads, duration.count()/1000.0});
-    }
-    std::ofstream out_file("strong_scalability.txt");
-    for (const auto& el : results_table)
-    {
-        out_file << std::get<0>(el) << " " << std::get<1>(el) << std::endl;
-    }
-    out_file.close();
+        std::ofstream out_file("strong_scalability.txt");
+        for (const auto& el : results_table)
+        {
+            out_file << std::get<0>(el) << " " << std::get<1>(el) << std::endl;
+        }
+        out_file.close();
+    #else
+        std::cout << "[!ERROR!] Strong scalability test is unavailable: compile with -fopenmp" << std::endl;
+    #endif
 }
 
