7a7d357b216a70acb526836cff430ae9187e7b90
mayhem <andrea10.grassi@mail.polimi.it>
2024-02-25 15:10:07 +0000

    d2q9

--- llalbm/src/utils/aliases.hpp
+++ llalbm/src/utils/aliases.hpp
@@ -24,3 +24,25 @@ enum btypes {TOP_WALL, RIGHT_WALL, BOTTOM_WALL, LEFT_WALL, RIGHT_TOP_C, RIGHT_BO
 template<std::size_t d>
 using boundaryPoint = std::tuple<Point<d>, btypes>; 
 
+/// Velocity Sets
+/// D2Q9
+class vSets
+{
+private:
+    /* data */
+public:
+    Matrix<double,9,2> D2Q9;
+    Matrix<double,27,3> D3Q27;
+    vSets(){
+        D2Q9 << 0 , 0,
+                1 , 0,
+                0 , 1, 
+                -1, 0,
+                0 ,-1,
+                1 , 1,
+                -1, 1,
+                -1,-1,
+                1 ,-1;
+    }
+    
+};
