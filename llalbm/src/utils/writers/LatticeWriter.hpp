79c7b515cd8681702eb83b6db3aa4913120aee88
Lorenzo Fonnesu <75188388+Fonzy-01@users.noreply.github.com>
2024-03-16 18:02:29 +0100

    .bin to .txt

--- llalbm/src/utils/writers/LatticeWriter.hpp
+++ llalbm/src/utils/writers/LatticeWriter.hpp
@@ -39,12 +39,12 @@ namespace llalbm::util::writer
         }else
         {
             logger.info("-> results directory already exists");
-            logger.info("clearing the directory (only .bin files)");
+            logger.info("clearing the directory");
 
             for (const auto& entry : std::filesystem::directory_iterator(output_dir_path))
             {
                 std::cout << entry.path();
-                if (entry.path().extension() == ".bin")
+                if (entry.path().extension() == ".bin" || entry.path().extension() == ".txt")
                 {
                     std::filesystem::remove(entry.path());
                     std::cout<< " removed" << std::endl;
@@ -58,19 +58,19 @@ namespace llalbm::util::writer
         const Tensor<double, dim + 1>& global_u, 
         std::size_t iteration_count)
     {
-        std::string output_file_path_u_x = output_dir_path + "/output-" + std::to_string(iteration_count) + "u_x.bin";
-        std::ofstream output_file_u_x(output_file_path_u_x, std::ios::binary);
+        std::string output_file_path_u_x = output_dir_path + "/output-" + std::to_string(iteration_count) + "u_x.txt";
+        std::ofstream output_file_u_x(output_file_path_u_x);
 
-        std::string output_file_path_u_y = output_dir_path + "/output-" + std::to_string(iteration_count) + "u_y.bin";
-        std::ofstream output_file_u_y(output_file_path_u_y, std::ios::binary);
+        std::string output_file_path_u_y = output_dir_path + "/output-" + std::to_string(iteration_count) + "u_y.txt";
+        std::ofstream output_file_u_y(output_file_path_u_y);
 
         assert(output_file_u_x.is_open() && "ERROR: u_x file could not be opened");
         assert(output_file_u_y.is_open() && "ERROR: u_y file could not be opened");
 
         for (Eigen::Index i = 0; i < global_u.dimension(0); ++i) {
             for (Eigen::Index j = 0; j < global_u.dimension(1); ++j) {
-                output_file_u_x.write(reinterpret_cast<const char*>(&global_u(i, j, 0)), sizeof(double));
-                output_file_u_y.write(reinterpret_cast<const char*>(&global_u(i, j, 1)), sizeof(double));
+                output_file_u_x << global_u(i, j, 0) << " ";
+                output_file_u_y << global_u(i, j, 1) << " ";
             }
         }
 
